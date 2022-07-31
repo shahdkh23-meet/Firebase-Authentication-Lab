9f4ce5925642d9c82f32f4a85c9015bce003cab1
shahdkh23-meet <109900082+shahdkh23-meet@users.noreply.github.com>
2022-07-31 14:30:26 +0300

    112233

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,8 +2,28 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {const firebaseConfig = {
+
+  "apiKey": "AIzaSyCftFrZbHcMC0dz2XqzVva0B1RXnBp9qCk",
+
+  "authDomain": "first-try-firebase-6e2dd.firebaseapp.com",
+
+  "projectId": "first-try-firebase-6e2dd",
+
+  "storageBucket": "first-try-firebase-6e2dd.appspot.com",
+
+  "messagingSenderId": "572341558806",
+
+  "appId": "1:572341558806:web:acacd3a8dab8d879fbdf37",
+
+  "measurementId": "G-TQTQ9ZV9CN"
+  } 
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
+
+s
 
 
 @app.route('/', methods=['GET', 'POST'])
